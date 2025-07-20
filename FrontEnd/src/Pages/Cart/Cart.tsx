ac17d321b943dbaac79eb3734ad90a18899e3517
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-07-20 22:05:09 +0330

    feat : add cart item center section template

--- FrontEnd/src/Pages/Cart/Cart.tsx
+++ FrontEnd/src/Pages/Cart/Cart.tsx
@@ -5,7 +5,11 @@ import Service from "../../Components/Service/Service";
 import { FaMoneyBill1Wave, FaTruckFast } from "react-icons/fa6";
 import { BsBoxFill } from "react-icons/bs";
 import { TfiHeadphoneAlt } from "react-icons/tfi";
-import { MdPhoneInTalk } from "react-icons/md";
+import { MdMergeType, MdPhoneInTalk, MdWork } from "react-icons/md";
+import { GiMaterialsScience } from "react-icons/gi";
+import { RxValue } from "react-icons/rx";
+import { GoPackage } from "react-icons/go";
+import { FaChevronDown } from "react-icons/fa";
 
 const Cart = () => {
   return (
@@ -19,7 +23,69 @@ const Cart = () => {
         </section>
         <section className="flex justify-between">
           <div></div>
-          <div></div>
+          <div>
+            <div>
+              <div>
+                <h1>دانه قهوه باکسی‌برند Boxillian</h1>
+                <p>دانه قهوه | برند‌:‌Boxillian</p>
+              </div>
+              <div>
+                <h3>ویژگی های محصول</h3>
+                <ul>
+                  <li>
+                    <GiMaterialsScience />
+                    <span>جنس محصول:</span>
+                    <span>پودر قهوه</span>
+                  </li>
+                  <li>
+                    <MdWork />
+                    <span>کارایی:</span>
+                    <span>استفاده روزمره</span>
+                  </li>
+                  <li>
+                    <RxValue />
+                    <span>اندازه:</span>
+                    <span>۵۰۰ گرم</span>
+                  </li>
+                  <li>
+                    <MdMergeType />
+                    <span>نوع محصول:</span>
+                    <span>روبوستا</span>
+                  </li>
+                  <li>
+                    <GoPackage />
+                    <span>نوع بسته بندی:</span>
+                    <span>کاغذی</span>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <h3>یک گزینه‌را انتخاب کنید:</h3>
+                <div>
+                  <span>مقدار:</span>
+                  <div>
+                    <span>۱کیلوگرم</span>
+                    <FaChevronDown />
+                    <div className="invisible"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <h2>
+                ۲۴۰،۰۰۰<span>تومان</span>
+              </h2>
+
+              <div>
+                <div>
+                  <button>+</button>
+                  <span>۱</span>
+                  <button>-</button>
+                </div>
+                <button>افزودن به سبد خرید</button>
+              </div>
+            </div>
+          </div>
           <div className="services-alignment">
             <div className="services-alignment">
               <Service title="شرایط ارسال" desc="حدود ۲ الی ۶ روز کاری">
@@ -42,7 +108,9 @@ const Cart = () => {
 
               <p>نیاز به مشاوره دارید؟</p>
               <div className="flex flex-row-reverse items-center ">
-                <h2 className="font-iran-yekan-bold text-primary text-3xl">-۰۲۱</h2>
+                <h2 className="font-iran-yekan-bold text-primary text-3xl">
+                  -۰۲۱
+                </h2>
                 <p className="font-iran-yekan text-xl">۱۲۳۲۳۲۴۳۴</p>
               </div>
             </div>

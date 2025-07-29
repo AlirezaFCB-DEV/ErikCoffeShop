7f5ed2e9c89ee60766eb2f962d66a283308555c9
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-07-29 16:36:20 +0330

    upaditing cart styles

--- FrontEnd/src/Components/Home_Main/Home_Main.tsx
+++ FrontEnd/src/Components/Home_Main/Home_Main.tsx
@@ -4,7 +4,7 @@ import ProductCategory from "./ProductsCategory/ProductsCategory";
 import SpecialOffsContainer from "./SpecialOffsContainer/SpecialOffsContainer";
 import TrainingPageContainer from "./TrainingsPage/TrainingPageContainer";
 
-const Main = () => {
+const Home_Main = () => {
   return (
     <main className="container mx-auto flex flex-col gap-24 max-[630px]:overflow-hidden  max-[630px]:justify-items-stretch pb-92 ">
       <ProductCategory />
@@ -16,4 +16,4 @@ const Main = () => {
   );
 };
 
-export default Main;
+export default Home_Main;
--- FrontEnd/src/Pages/Cart/Cart.tsx
+++ FrontEnd/src/Pages/Cart/Cart.tsx
@@ -1,11 +1,8 @@
 import { IoHome } from "react-icons/io5";
 import HeaderTop from "../../Components/Header/HeaderTop/HeaderTop";
 import NavigationMenu from "../../Components/NavigationMenu/NavigationMenu";
-import Service from "../../Components/Service/Service";
-import { FaMoneyBill1Wave, FaTruckFast } from "react-icons/fa6";
-import { BsBoxFill } from "react-icons/bs";
-import { TfiFullscreen, TfiHeadphoneAlt } from "react-icons/tfi";
-import { MdMergeType, MdPhoneInTalk, MdWork } from "react-icons/md";
+
+import { MdMergeType, MdWork } from "react-icons/md";
 import { GiMaterialsScience } from "react-icons/gi";
 import { RxValue } from "react-icons/rx";
 import { GoPackage } from "react-icons/go";
@@ -13,8 +10,8 @@ import { FaMinus, FaPlus } from "react-icons/fa";
 import Button from "../../Components/Button/Button";
 import { Link } from "react-router-dom";
 import Off_Percent from "../../Components/Off_Percent/Off_Percent";
-import { HiDotsHorizontal } from "react-icons/hi";
 import Footer from "../../Components/Footer/Footer";
+import { TiShoppingCart } from "react-icons/ti";
 
 interface ProductCountBtns {
   button: React.ReactNode;
@@ -37,54 +34,16 @@ const Cart = () => {
           </HeaderTop>
           <NavigationMenu />
         </section>
-        <section className="flex gap-6 relative items-start">
+        <section className="flex gap-6 relative items-start i">
           <div className="flex-3 flex flex-col gap-16">
             <div className="flex gap-8 h-full w-full relative">
-              <div className=" flex flex-2 flex-col gap-6">
-                <div className="w-full flex-5 border-stroke border-2 rounded-xl p-4 relative">
-                  <Off_Percent percent={20} />
-                  <img
-                    src="/images/Products/product1.png"
-                    className="w-full max-h-100"
-                    alt="محصول"
-                  />
-                  <button className="absolute right-4 bottom-4 bg-secondary cursor-pointer p-4 rounded-full hover:bg-primary transition-colors group">
-                    <TfiFullscreen className="text-primary group-hover:text-secondary" />
-                  </button>
-                </div>
-                <div className="flex flex-1 gap-4">
-                  <div className="flex-1 flex items-center justify-center border-stroke border-2 rounded-lg cursor-pointer hover:border-primary transition-colors">
-                    <HiDotsHorizontal className="text-3xl text-sec-text" />
-                  </div>
-                  <div className="flex-1 flex items-center justify-center border-stroke border-2 rounded-lg cursor-pointer hover:border-primary transition-colors">
-                    <img
-                      src="/images/Products/product1.png"
-                      className="w-14 h-16"
-                      alt="محصول"
-                    />
-                  </div>
-                  <div className="flex-1 flex items-center justify-center border-stroke border-2 rounded-lg cursor-pointer hover:border-primary transition-colors">
-                    <img
-                      src="/images/Products/product1.png"
-                      className="w-14 h-16"
-                      alt="محصول"
-                    />
-                  </div>
-                  <div className="flex-1 flex items-center justify-center border-stroke border-2 rounded-lg cursor-pointer hover:border-primary transition-colors">
-                    <img
-                      src="/images/Products/product1.png"
-                      className="w-14 h-16"
-                      alt="محصول"
-                    />
-                  </div>
-                  <div className="flex-1 flex items-center justify-center border-stroke border-2 rounded-lg cursor-pointer hover:border-primary transition-colors">
-                    <img
-                      src="/images/Products/product1.png"
-                      className="w-14 h-16"
-                      alt="محصول"
-                    />
-                  </div>
-                </div>
+                <div className=" flex flex-1/5 justify-center relative rounded-xl border-stroke border-2">
+                <Off_Percent percent={20} />
+                <img
+                  src="/images/Products/product1.png"
+                  className="w-full max-h-90"
+                  alt="محصول"
+                />
               </div>
               <div className="flex flex-3  flex-col justify-between items-start">
                 <div className="flex flex-col gap-6">
@@ -95,66 +54,160 @@ const Cart = () => {
                     دانه قهوه | برند‌:‌Boxillian
                   </p>
                 </div>
-                <div className="flex flex-col gap-4">
-                  <h3 className="font-morabba-bold">ویژگی های محصول</h3>
-                  <ul className="flex  flex-col gap-6 border-r-3 border-stroke pr-4 relative">
-                    <li className="product-properties">
-                      <GiMaterialsScience className="text-xl" />
-                      <span className="text-sec-text text-sm">جنس محصول:</span>
-                      <span className="text-sm">پودر قهوه</span>
-                    </li>
-                    <li className="product-properties">
-                      <MdWork className="text-xl" />
-                      <span className="text-sec-text text-sm">کارایی:</span>
-                      <span className="text-sm">استفاده روزمره</span>
-                    </li>
-                    <li className="product-properties">
-                      <RxValue className="text-xl" />
-                      <span className="text-sec-text text-sm">اندازه:</span>
-                      <span className="text-sm">۵۰۰ گرم</span>
-                    </li>
-                    <li className="product-properties">
-                      <MdMergeType className="text-xl" />
-                      <span className="text-sec-text text-sm">نوع محصول:</span>
-                      <span className="text-sm">روبوستا</span>
-                    </li>
-                    <li className="product-properties">
-                      <GoPackage className="text-xl" />
-                      <span className="text-sec-text text-sm">
-                        نوع بسته بندی:
-                      </span>
-                      <span className="text-sm">کاغذی</span>
-                    </li>
-                    <div className="bg-primary absolute w-[3px] h-14 -right-[3px]"></div>
-                  </ul>
+
+                <ul className="flex  flex-col gap-4">
+                  <li className="product-properties">
+                    <GiMaterialsScience className="text-xl" />
+                    <span className="text-sec-text text-sm">جنس محصول:</span>
+                    <span className="text-sm">پودر قهوه</span>
+                  </li>
+                  <li className="product-properties">
+                    <MdWork className="text-xl" />
+                    <span className="text-sec-text text-sm">کارایی:</span>
+                    <span className="text-sm">استفاده روزمره</span>
+                  </li>
+                  <li className="product-properties">
+                    <RxValue className="text-xl" />
+                    <span className="text-sec-text text-sm">اندازه:</span>
+                    <span className="text-sm">۵۰۰ گرم</span>
+                  </li>
+                  <li className="product-properties">
+                    <MdMergeType className="text-xl" />
+                    <span className="text-sec-text text-sm">نوع محصول:</span>
+                    <span className="text-sm">روبوستا</span>
+                  </li>
+                  <li className="product-properties">
+                    <GoPackage className="text-xl" />
+                    <span className="text-sec-text text-sm">
+                      نوع بسته بندی:
+                    </span>
+                    <span className="text-sm">کاغذی</span>
+                  </li>
+                </ul>
+                <div className="flex gap-2 items-center">
+                  <span className="text-sec-text ">مقدار:</span>
+                  <div className="flex flex-col items-center border-stroke border-2 w-50 relative py-2.5 px-3 rounded-xl">
+                    <span className="text-2xl">۱</span>
+                    <div className="flex w-full justify-around">
+                      {countBtns.map((btn) => (
+                        <Button
+                          className="border-stroke border-2 py-1 px-3 rounded-lg cursor-pointer group hover:border-stone-700 transition-colors"
+                          key={crypto.randomUUID()}
+                        >
+                          {btn.button}
+                        </Button>
+                      ))}
+                    </div>
+                  </div>
                 </div>
-                <div className="flex flex-col gap-4">
-                  <h3 className="font-morabba-bold">
-                    یک گزینه‌را انتخاب کنید:
-                  </h3>
-                  <div className="flex gap-2 items-center">
-                    <span className="text-sec-text ">مقدار:</span>
-                    <div className="flex flex-col items-center border-stroke border-2 w-50 relative py-2.5 px-3 rounded-xl">
-                      <span className="text-2xl">۱</span>
-                      <div className="flex w-full justify-around">
-                        {countBtns.map((btn) => (
-                          <Button
-                            className="border-stroke border-2 py-1 px-3 rounded-lg cursor-pointer group hover:border-stone-700 transition-colors"
-                            key={crypto.randomUUID()}
-                          >
-                            {btn.button}
-                          </Button>
-                        ))}
-                      </div>
+              </div>
+              <div className="w-full h-0.5 absolute -bottom-8 bg-stroke"></div>
+            </div>
+            <div className="flex gap-8 h-full w-full relative">
+              <div className=" flex flex-1/5 justify-center relative rounded-xl border-stroke border-2">
+                <Off_Percent percent={20} />
+                <img
+                  src="/images/Products/product1.png"
+                  className="w-full max-h-90"
+                  alt="محصول"
+                />
+              </div>
+              <div className="flex flex-3  flex-col justify-between items-start">
+                <div className="flex flex-col gap-6">
+                  <h2 className="font-iran-yekan-bold text-2xl">
+                    دانه قهوه باکسی‌برند Boxillian
+                  </h2>
+                  <p className="text-sm text-sec-text">
+                    دانه قهوه | برند‌:‌Boxillian
+                  </p>
+                </div>
+
+                <ul className="flex  flex-col gap-4">
+                  <li className="product-properties">
+                    <GiMaterialsScience className="text-xl" />
+                    <span className="text-sec-text text-sm">جنس محصول:</span>
+                    <span className="text-sm">پودر قهوه</span>
+                  </li>
+                  <li className="product-properties">
+                    <MdWork className="text-xl" />
+                    <span className="text-sec-text text-sm">کارایی:</span>
+                    <span className="text-sm">استفاده روزمره</span>
+                  </li>
+                  <li className="product-properties">
+                    <RxValue className="text-xl" />
+                    <span className="text-sec-text text-sm">اندازه:</span>
+                    <span className="text-sm">۵۰۰ گرم</span>
+                  </li>
+                  <li className="product-properties">
+                    <MdMergeType className="text-xl" />
+                    <span className="text-sec-text text-sm">نوع محصول:</span>
+                    <span className="text-sm">روبوستا</span>
+                  </li>
+                  <li className="product-properties">
+                    <GoPackage className="text-xl" />
+                    <span className="text-sec-text text-sm">
+                      نوع بسته بندی:
+                    </span>
+                    <span className="text-sm">کاغذی</span>
+                  </li>
+                </ul>
+                <div className="flex gap-2 items-center">
+                  <span className="text-sec-text ">مقدار:</span>
+                  <div className="flex flex-col items-center border-stroke border-2 w-50 relative py-2.5 px-3 rounded-xl">
+                    <span className="text-2xl">۱</span>
+                    <div className="flex w-full justify-around">
+                      {countBtns.map((btn) => (
+                        <Button
+                          className="border-stroke border-2 py-1 px-3 rounded-lg cursor-pointer group hover:border-stone-700 transition-colors"
+                          key={crypto.randomUUID()}
+                        >
+                          {btn.button}
+                        </Button>
+                      ))}
                     </div>
                   </div>
                 </div>
               </div>
               {/* <div className="w-full h-0.5 absolute -bottom-8 bg-stroke"></div> */}
             </div>
-
           </div>
-          <div className="sticky top-5 services-alignment flex-1">
+          <div className="bg-[#3d261d] text-white flex-1 sticky top-5 p-10 rounded-tl-4xl rounded-br-4xl flex flex-col gap-6 overflow-hidden">
+            <div className="text-center relative">
+              <TiShoppingCart className="w-full text-3xl" />
+              <h2 className="font-morabba-bold text-2xl">سبد خرید شما</h2>
+              <div className="bg-stroke w-full h-1 rounded-2xl absolute -bottom-2.5 "></div>
+            </div>
+            <div className="flex flex-col gap-4 font-morabba text-xl">
+              <div className="flex justify-between items-center">
+                <h4>قیمت کالا ها (۲کالا)</h4>
+                <span>تومان ۱۹۹،۰۰۰ </span>
+              </div>
+              <div className="flex justify-between items-center">
+                <h4>قیمت کالا ها (۲کالا)</h4>
+                <span>تومان ۱۹۹،۰۰۰ </span>
+              </div>
+              <div className="flex justify-between items-center">
+                <h4>قیمت کالا ها (۲کالا)</h4>
+                <span>تومان ۱۹۹،۰۰۰ </span>
+              </div>
+            </div>
+
+            <Link
+              to="shipping"
+              className="w-full text-center p-2.5 bg-secondary rounded-tl-2xl rounded-br-2xl hover:bg-primary transition-colors group"
+            >
+              <span className="text-xl text-primary font-morabba-bold transition-colors group-hover:text-secondary ">
+                ادامه خرید
+              </span>
+            </Link>
+
+            <div className="shipping_circles -top-6 left-1/2 right-1/2 translate-x-1/2"></div>
+            <div className="shipping_circles left-1/2 right-1/2 translate-x-1/2 -bottom-6"></div>
+            <div className="shipping_circles top-1/2 bottom-1/2 -translate-y-1/2 -left-6"></div>
+            <div className="shipping_circles  -right-6 -translate-y-1/2  bottom-1/2 top-1/2"></div>
+          </div>
+          {/* 
+          <div className="services-alignment ">
             <div className="services-alignment">
               <Service title="شرایط ارسال" desc="حدود ۲ الی ۶ روز کاری">
                 <FaTruckFast className="service-icon" />
@@ -182,7 +235,7 @@ const Cart = () => {
                 <p className="font-iran-yekan text-xl">۱۲۳۲۳۲۴۳۴</p>
               </div>
             </div>
-          </div>
+          </div> */}
         </section>
       </div>
       <Footer />

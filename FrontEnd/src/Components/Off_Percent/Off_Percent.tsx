8b8c585fcf91f425cf288229a946e68bac67827e
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-07-29 16:36:20 +0330

    upaditing cart styles

--- FrontEnd/src/Components/Off_Percent/Off_Percent.tsx
+++ FrontEnd/src/Components/Off_Percent/Off_Percent.tsx
@@ -2,7 +2,7 @@ import type OffPercent from "../../Types/Off_Percent.type";
 
 const Off_Percent = ({ percent }: OffPercent) => {
   return (
-    <span className="absolute left-5 text-lg bg-black text-white px-3 py-0.5 rounded-full">
+    <span className="absolute left-5 top-5 text-lg bg-black text-white px-3 py-0.5 rounded-full">
       {percent}٪
     </span>
   );
--- FrontEnd/src/Pages/Home/Home.tsx
+++ FrontEnd/src/Pages/Home/Home.tsx
@@ -1,15 +1,15 @@
 import Footer from "../../Components/Footer/Footer";
 import Header from "../../Components/Header/Header";
-import Main from "../../Components/Main/Main";
- 
+import Home_Main from "../../Components/Home_Main/Home_Main";
+
 const Home = () => {
   return (
     <>
       <Header />
-      <Main />
+      <Home_Main />
       <Footer />
     </>
   );
 };
 
-export default Home
+export default Home;

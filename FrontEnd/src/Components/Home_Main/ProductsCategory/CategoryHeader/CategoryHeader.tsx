28d225dd5c2decd3f1a9b079d2c0175a30275916
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-07-31 13:39:35 +0330

    update: update specail_offs comp and category items comp

--- FrontEnd/src/Components/Home_Main/ProductsCategory/CategoryHeader/CategoryHeader.tsx
+++ FrontEnd/src/Components/Home_Main/ProductsCategory/CategoryHeader/CategoryHeader.tsx
@@ -1,24 +1,14 @@
 import { BiSolidCoffeeBean } from "react-icons/bi";
-import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
-import Button from "../../../Button/Button";
 
 const CategoryHeader = () => {
   return (
-    <section className="flex justify-between w-full items-center ">
+    <section className="flex justify-center items-center ">
       <div className="flex items-center gap-4">
         <BiSolidCoffeeBean className="text-primary text-7xl max-[630px]:text-4xl" />
         <div className="leading-10">
           <h2 className="font-morabba-bold text-3xl max-[630px]:text-2xl">دسته‌بندی‌محصولات</h2>
           <p className="max-[630px]:text-sm">از میان بهترین‌ ها ، بهترین هارا برای شما فراهم کردیم!</p>
         </div>
-      </div>
-      <div className="flex gap-4 max-[630px]:hidden">
-        <Button className="category-btn group">
-          <BsChevronRight className="category-btn-icon" />
-        </Button>
-        <Button className="category-btn group">
-          <BsChevronLeft className="category-btn-icon" />
-        </Button>
       </div>
     </section>
   );

4c2ecdfdea8ac89867d7c0aba6fdfbea60d87e4c
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-08-03 21:09:42 +0330

    feat: tutorail page is complete

---
+++ FrontEnd/src/Pages/Articles/Tutorials.tsx
@@ -0,0 +1,68 @@
+import { Link } from "react-router-dom";
+import Footer from "../../Components/Footer/Footer";
+import Short_Header from "../../Components/Short_Header/Short_Header";
+import Article from "./Article/Article";
+import Sidebar_Article from "./Sidebar_Article/Sidebar_Article";
+
+const Articles = () => {
+  return (
+    <>
+      <div className="container">
+        <Short_Header />
+      </div>
+      <div className="flex flex-row-reverse pl-16 pr-8 ">
+        <aside className="min-h-200 flex-2 tutorial_page_shadow rounded-2xl">
+          <h2>تازه ترین</h2>
+
+          <ul className="flex flex-col gap-8 px-4">
+            <li>
+              <Link to="articles/article-1">
+                <Sidebar_Article />
+              </Link>
+            </li>
+            <li>
+              <Link to="articles/article-1">
+                <Sidebar_Article />
+              </Link>
+            </li>
+            <li>
+              <Link to="articles/article-1">
+                <Sidebar_Article />
+              </Link>
+            </li>
+            <li>
+              <Link to="articles/article-1">
+                <Sidebar_Article />
+              </Link>
+            </li>
+          </ul>
+        </aside>
+
+        <div className="flex flex-7 flex-wrap gap-8">
+          <Link to="title">
+            <Article />
+          </Link>
+          <Link to="title">
+            <Article />
+          </Link>
+          <Link to="title">
+            <Article />
+          </Link>
+          <Link to="title">
+            <Article />
+          </Link>
+          <Link to="title">
+            <Article />
+          </Link>
+          <Link to="title">
+            <Article />
+          </Link>
+        </div>
+      </div>
+
+      <Footer />
+    </>
+  );
+};
+
+export default Articles;

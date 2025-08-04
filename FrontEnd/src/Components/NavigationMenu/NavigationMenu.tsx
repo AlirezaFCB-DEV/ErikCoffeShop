6717f6bd0397c3e09f783530e7e912f4b2379155
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-08-04 14:02:03 +0330

    feat : add and style drop down

--- FrontEnd/src/Components/NavigationMenu/NavigationMenu.tsx
+++ FrontEnd/src/Components/NavigationMenu/NavigationMenu.tsx
@@ -2,37 +2,38 @@ import { FaInstagram } from "react-icons/fa";
 import { IoIosArrowDown } from "react-icons/io";
 import { RiCircleFill } from "react-icons/ri";
 import { Link } from "react-router-dom";
+import Drop_Down from "./Drop_Down/Drop_Down";
 
 const NavigationMenu = () => {
   return (
-    <nav className="w-full flex justify-between pl-4 pr-15 min-h-17 items-center rounded-full bg-txt text-white max-[630px]:hidden">
-      <ul className="flex items-center gap-10">
-        <li>
+    <nav className="w-full flex justify-between pl-4 pr-15 h-17 rounded-full bg-txt text-white max-[630px]:hidden relative">
+      <ul className="flex gap-10">
+        <li className="nav_menu_items">
           <Link to="/" className="flex items-center gap-1">
             <RiCircleFill className="text-[10px]" />
-            <span className="">صفحه اصلی</span>
+            <span>صفحه اصلی</span>
           </Link>
         </li>
-        <li className="flex items-center gap-0.5">
+        <li className="nav_menu_items gap-0.5 group">
           لیست محصولات
           <IoIosArrowDown></IoIosArrowDown>
-          <ul className="hidden">
-
-          </ul>
+          <Drop_Down />
         </li>
-        <li>
+        <li className="nav_menu_items">
           <Link to="/tutorials">آموزش ها</Link>
         </li>
-        <li>
+        <li className="nav_menu_items">
           <Link to="/about">درباره</Link>
         </li>
-        <li>
+        <li className="nav_menu_items">
           <Link to="/contact">تماس باما</Link>
         </li>
       </ul>
-      <div className="flex bg-white-20 p-2.5 rounded-full items-center gap-1">
-        <span className="text-sm mt-0.5">Erick.Cafe</span>
-        <FaInstagram className="text-2xl" />
+      <div className="h-full flex items-center">
+        <div className="flex bg-white-20 p-2.5 rounded-full items-center gap-1">
+          <span className="text-sm mt-0.5">Erick.Cafe</span>
+          <FaInstagram className="text-2xl" />
+        </div>
       </div>
     </nav>
   );

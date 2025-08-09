23da60eed414bbc37f99b18dc1f80ddfe72efbdf
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-08-09 17:31:02 +0330

    fix : fixed responsive bugs

--- FrontEnd/src/Components/Nav_List/Nav_List.tsx
+++ FrontEnd/src/Components/Nav_List/Nav_List.tsx
@@ -4,25 +4,25 @@ import Drop_Down from "../NavigationMenu/Drop_Down/Drop_Down";
 
 const Nav_List = () => {
   return (
-    <ul className="flex gap-10 select-none max-lg:gap-6 max-md:gap-4 max-sm:flex-col max-sm:gap-12">
+    <ul className="flex gap-10 select-none max-lg:gap-6 max-md:gap-4 max-sm:flex-col max-sm:gap-12 ">
       <li className="nav_menu_items nav_active_item">
-        <Link to="/" className="flex items-center gap-1">
+        <Link to="/" className="flex items-center gap-1 link">
           <span>صفحه اصلی</span>
         </Link>
       </li>
       <li className="nav_menu_items gap-0.5 group">
-        <Link to="/products">لیست محصولات</Link>
+        <Link to="/products"className="link">لیست محصولات</Link>
         <IoIosArrowDown className="rotate-180 group-hover:rotate-0 transition-all duration-300 max-md:hidden"></IoIosArrowDown>
         <Drop_Down />
       </li>
       <li className="nav_menu_items">
-        <Link to="/tutorials">آموزش ها</Link>
+        <Link to="/tutorials" className="link">آموزش ها</Link>
       </li>
       <li className="nav_menu_items">
-        <Link to="/about">درباره</Link>
+        <Link to="/about" className="link">درباره</Link>
       </li>
       <li className="nav_menu_items">
-        <Link to="/contact">تماس باما</Link>
+        <Link to="/contact" className="link">تماس باما</Link>
       </li>
     </ul>
   );

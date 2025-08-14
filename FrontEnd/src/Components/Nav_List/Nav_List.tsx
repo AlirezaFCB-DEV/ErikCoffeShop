5ef3a2be18a1ea1ff2788cd02197819f71c09ae9
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-08-14 15:30:25 +0330

    feat(nav): add active menu item highlighting based on current route

--- FrontEnd/src/Components/Nav_List/Nav_List.tsx
+++ FrontEnd/src/Components/Nav_List/Nav_List.tsx
@@ -1,29 +1,48 @@
 import { IoIosArrowDown } from "react-icons/io";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Drop_Down from "../NavigationMenu/Drop_Down/Drop_Down";
+import { useEffect, useState } from "react";
+import type list_template_props from "../../Types/List_Template_Props.type";
 
 const Nav_List = () => {
+  const [templates, set_tmps] = useState<list_template_props[]>([
+    { id: 1, route: "/", title: "صفحه اصلی", active: true },
+    { id: 2, route: "/products", title: "لیست محصولات", active: false },
+    { id: 3, route: "/tutorials", title: "آموزش ها", active: false },
+    { id: 4, route: "/about", title: "درباره", active: false },
+    { id: 5, route: "/contact", title: "تماس باما", active: false },
+  ]);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    const current_path = location.pathname.split("/")[1];
+
+    set_tmps((prev) =>
+      prev.map((tmp) => ({ ...tmp, active: tmp.route === `/${current_path}` }))
+    );
+  }, [location.pathname]);
+
   return (
     <ul className="flex gap-10 select-none max-lg:gap-6 max-md:gap-4 max-sm:flex-col max-sm:gap-12 ">
-      <li className="nav_menu_items nav_active_item">
-        <Link to="/" className="flex items-center gap-1 link">
-          <span>صفحه اصلی</span>
-        </Link>
-      </li>
-      <li className="nav_menu_items gap-0.5 group">
-        <Link to="/products"className="link">لیست محصولات</Link>
-        <IoIosArrowDown className="rotate-180 group-hover:rotate-0 transition-all duration-300 max-md:hidden"></IoIosArrowDown>
-        <Drop_Down />
-      </li>
-      <li className="nav_menu_items">
-        <Link to="/tutorials" className="link">آموزش ها</Link>
-      </li>
-      <li className="nav_menu_items">
-        <Link to="/about" className="link">درباره</Link>
-      </li>
-      <li className="nav_menu_items">
-        <Link to="/contact" className="link">تماس باما</Link>
-      </li>
+      {templates.map((tmp) => (
+        <li
+          className={`nav_menu_items ${
+            tmp.active ? "nav_active_item" : "nav_inactive_item"
+          }  ${tmp.title === "لیست محصولات" && " gap-0.5 group"}`}
+          key={tmp.id}
+        >
+          <Link to={tmp.route} className="link">
+            {tmp.title}
+          </Link>
+          {tmp.title === "لیست محصولات" && (
+            <>
+              <IoIosArrowDown className="rotate-180 group-hover:rotate-0 transition-all duration-300 max-md:hidden"></IoIosArrowDown>
+              <Drop_Down />
+            </>
+          )}
+        </li>
+      ))}
     </ul>
   );
 };
---
+++ FrontEnd/src/Types/List_Template_Props.type.ts
@@ -0,0 +1,6 @@
+export default interface list_template_props {
+  id: number;
+  route: `/${string}`;
+  title: string;
+  active: boolean;
+}

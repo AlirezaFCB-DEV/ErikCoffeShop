c6d62e8be444d76a5200077356c228a6fab7b963
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2025-12-24 13:30:53 +0330

    update : move user manager in managers.py

--- BackEnd/Account/models.py
+++ BackEnd/Account/models.py
@@ -1,31 +1,9 @@
-from django.contrib.auth.models import AbstractBaseUser , BaseUserManager , PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser  , PermissionsMixin
 from django.db import models
+from .managers import CustomUserManager
 
 # Create your models here.
 
-class CustomUserManager(BaseUserManager) :
-    def create_user (self , phone_number , password=None) :
-        if not phone_number :
-            raise ValueError("User must have a phone number")
-        
-        user = self.model(phone_number=phone_number)
-        
-        user.set_password(password)
-        
-        user.save(using=self._db)
-        
-        return user
-    
-    def create_superuser (self , phone_number , password=None) :
-        
-        user = self.create_user(phone_number , password=password)
-        
-        user.is_admin = True
-        user.is_superuser = True
-        user.is_staff = True
-        
-        user.save(using=self._db)
-        return user
 class CustomUser(AbstractBaseUser , PermissionsMixin) :
     phone_number = models.CharField(max_length=11 , unique=True)
     

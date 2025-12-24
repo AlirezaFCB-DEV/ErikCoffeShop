f2b747eb4962cdfb9f9ee5e6fbe1e407e9c09eb8
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2025-12-24 13:27:10 +0330

    update: custom user

--- BackEnd/Account/models.py
+++ BackEnd/Account/models.py
@@ -1,18 +1,18 @@
-from django.contrib.auth.models import AbstractBaseUser , BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser , BaseUserManager , PermissionsMixin
 from django.db import models
 
 # Create your models here.
 
-class Custom_User_Manager(BaseUserManager) :
+class CustomUserManager(BaseUserManager) :
     def create_user (self , phone_number , password=None) :
         if not phone_number :
-            raise ValueError("User must have an email address")
+            raise ValueError("User must have a phone number")
         
         user = self.model(phone_number=phone_number)
         
         user.set_password(password)
         
-        user.save(using=self.db)
+        user.save(using=self._db)
         
         return user
     
@@ -21,23 +21,29 @@ class Custom_User_Manager(BaseUserManager) :
         user = self.create_user(phone_number , password=password)
         
         user.is_admin = True
-        user.save(using=self.db)
+        user.is_superuser = True
+        user.is_staff = True
+        
+        user.save(using=self._db)
         return user
-class Custom_User(AbstractBaseUser) :
-    phone_number = models.CharField(max_length=11)
+class CustomUser(AbstractBaseUser , PermissionsMixin) :
+    phone_number = models.CharField(max_length=11 , unique=True)
     
     is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False) 
+    is_staff = models.BooleanField(default=False)
     
-    objects = Custom_User_Manager()
+    objects = CustomUserManager()
     
     USERNAME_FIELD = "phone_number"
+    
+    REQUIRED_FIELDS = []
     
     def __str__(self):
         return self.phone_number
     
-    def has_perm(self , perm , obj=None) :
-        return True
+    def has_perm(self, perm, obj = None):
+        return self.is_admin
     
-    def has_module_perms(self , app_label) :
+    def has_module_perms(self, app_label):
         return True

382ab165b805fe318c9aca79adb34aae21bce66d
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2026-01-07 12:00:22 +0330

    update : refactor otp verify system

--- BackEnd/Account/backends.py
+++ BackEnd/Account/backends.py
@@ -1,13 +1,11 @@
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
-from django.utils import timezone
-from datetime import timedelta
-
-from .models import Otp
+from .services import otp_verifier
 
 User = get_user_model()
 
 class PhoneNumberBackend(ModelBackend) :
+    
     def authenticate(self, request, phone_number = None, Otp_Code = None, **kwargs):
         username = kwargs.get("username")
         
@@ -17,32 +15,9 @@ class PhoneNumberBackend(ModelBackend) :
             user = User.objects.get(phone_number=identifier)
             
             if Otp_Code :
-                return user if self.otp_verifier(user , Otp_Code) else None
+                return user if otp_verifier(user , Otp_Code) else None
             
         except User.DoesNotExist :
-            return None                 
-            
-        
+            return None                     
         
         return None
-            
-    def otp_verifier(self , user_obj , otp_code) :
-        try :
-            otp_record = Otp.objects.get(user=user_obj)
-            if otp_record.code  == otp_code :
-                
-                if otp_record.created_at + timedelta(minutes=2) < timezone.now() or otp_record.attempts > 3:
-                    otp_record.delete()
-                    return False
-                
-                otp_record.delete()
-                return True
-            else :
-                otp_record.attempts += 1
-                otp_record.save()
-                
-                return False
-                    
-        except Otp.DoesNotExist :
-            return False
-        
--- BackEnd/Account/services.py
+++ BackEnd/Account/services.py
@@ -1,10 +1,37 @@
-import re 
+import re
 import random
+from django.utils import timezone
+from datetime import timedelta
 
-def is_phone(phone : str) :
+from .models import Otp
+
+
+def is_phone(phone: str):
     pattern = r"^(?:\+98|0)?9\d{9}$"
-    return re.match(pattern , phone)
+    return re.match(pattern, phone)
 
-def otp_generator() :
-        otp = str(random.randint(100000,999999))
-        return otp
+
+def otp_generator():
+    otp = str(random.randint(100000, 999999))
+    return otp
+
+
+def otp_verifier(self, user_obj, otp_code):
+    try:
+        otp_record = Otp.objects.get(user=user_obj)
+        if otp_record.code == otp_code:
+
+            if otp_record.created_at + timedelta(minutes=2) < timezone.now() or otp_record.attempts > 3:
+                otp_record.delete()
+                return False
+
+            otp_record.delete()
+            return True
+        else:
+            otp_record.attempts += 1
+            otp_record.save()
+
+            return False
+
+    except Otp.DoesNotExist:
+        return False
